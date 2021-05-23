0ebdd7a1d01daaf19576ce1c6a3775f16e88c8f9
Hang Zhou <zhouhang2991@gmail.com>
2021-05-22 18:58:56 -0700

    added .gitignore .gitattributes

---
+++ ImageComp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ImageComp
+//
+//  Created by Hang Zhou on 2021/5/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ImageComp/ImageCompApp.swift
@@ -0,0 +1,17 @@
+//
+//  ImageCompApp.swift
+//  ImageComp
+//
+//  Created by Hang Zhou on 2021/5/22.
+//
+
+import SwiftUI
+
+@main
+struct ImageCompApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
